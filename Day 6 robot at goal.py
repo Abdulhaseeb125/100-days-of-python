a744a39a7c4ba11f5a0cfa73561c2176744fded8
Abdul Haseeb <82708612+Abdulhaseeb125@users.noreply.github.com>
2022-12-17 20:35:13 +0500

    Update Day 6 robot at goal.py

--- Day 6 robot at goal.py
+++ Day 6 robot at goal.py
@@ -1,3 +1,6 @@
+#Note there are not the fastest codes because these are written on self thaughts not on guided these can be improved by thinking more for a while.
+#written by abdul haseeb
+
 #website link...
 https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
   
@@ -26,3 +29,27 @@ while not at_goal():
         while front_is_clear():
             move()
         turn_left()
+
+        
+# Maze code:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+       
+        
+def turn_around():
+    for i in range(0,3):
+        turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        while front_is_clear():
+            move()
+            if right_is_clear():
+                turn_right()
+    elif wall_in_front():
+        while wall_in_front() and  not wall_on_right():
+            turn_right()
+        while wall_in_front() and wall_on_right():
+            turn_left()
