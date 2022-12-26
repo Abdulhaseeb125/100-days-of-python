b43c9a497409953e4e843361ac9a40328315c7e3
Abdul Haseeb <82708612+Abdulhaseeb125@users.noreply.github.com>
2022-12-26 19:09:32 +0500

    Add files via upload

---
+++ Day_16_quiz.py
@@ -0,0 +1,45 @@
+from Day_16_Question import question_data
+
+
+class Quiz:
+    def __init__(self):
+        self.true_counter = 0
+        self.false_counter = 0
+        self.Again = True
+        self.input = None
+        self.total_questions = len(question_data)
+        self.asked = 0
+
+    def Counter(self):
+        print(f"Your Score is {self.true_counter}/{self.asked}")
+
+    def user_input(self):
+        while self.Again:
+            for question in question_data:
+                self.asked += 1
+                self.input = input(f'Q.No {self.asked}: {question["text"]}')
+                if self.asked == self.total_questions:
+                    self.Again = False
+                if self.input == question["answer"]:
+                    self.true_counter += 1
+                    print("You got it right!")
+                    print(f"The correct answer was {question['answer']}")
+                    self.Counter()
+                else:
+                    self.false_counter += 1
+                    print("That's Wrong..!")
+                    print(f"The correct answer was {question['answer']}")
+                    self.Counter()
+
+
+    def Completed(self):
+        print("You have completed the quiz.")
+        self.Counter()
+
+
+quiz = Quiz()
+
+quiz.user_input()
+print("\n")
+quiz.Completed()
+
