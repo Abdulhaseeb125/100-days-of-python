65aa59b1166a989c85b34d17aaeae97477d1bb29
Abdul Haseeb <82708612+Abdulhaseeb125@users.noreply.github.com>
2023-01-10 22:37:48 +0500

    Update main.py

--- Day 26 alphacode/main.py
+++ Day 26 alphacode/main.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-data = pd.read_csv("nato_phonetic_alphabet.csv")
-new_data ={row.letter:row.code for (index, row) in data.iterrows()}
+def alpha_geneartor():
+    data = pd.read_csv("nato_phonetic_alphabet.csv")
+    new_data ={row.letter:row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter the word:").upper()
-try:
-    code_list = [new_data[letter] for letter in user_input]
-except KeyError:
-    print("Please Enter valid data")
-else:
-    print(code_list)
+    user_input = input("Enter the word:").upper()
+    try:
+        code_list = [new_data[letter] for letter in user_input]
+    except KeyError:
+        print("Please Enter valid data")
+        alpha_generator()
+    else:
+        print(code_list)
+
+alpha_generator()
