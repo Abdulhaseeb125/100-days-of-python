5e8845a49f6a8ebea3bb02e55b77fcba860de53f
Abdul Haseeb <82708612+Abdulhaseeb125@users.noreply.github.com>
2023-01-10 22:34:00 +0500

    Update main.py

--- Day 26 alphacode/main.py
+++ Day 26 alphacode/main.py
@@ -4,5 +4,9 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 new_data ={row.letter:row.code for (index, row) in data.iterrows()}
 
 user_input = input("Enter the word:").upper()
-code_list = [new_data[letter] for letter in user_input]
-print(code_list)
+try:
+    code_list = [new_data[letter] for letter in user_input]
+except KeyError:
+    print("Please Enter valid data")
+else:
+    print(code_list)
